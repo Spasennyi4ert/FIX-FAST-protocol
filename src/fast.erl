4bd81cf80e894b6096366c76b90c1a4ac6178cf3
Spasennyi4ert <spasennyi4ert@gmail.com>
2014-09-03 21:10:34 +0400

    Update fast.erl

--- src/fast.erl
+++ src/fast.erl
@@ -37,3 +37,16 @@ handle_info( _Info, State) ->
   
 terminate(normal, State) ->
   {noreply, State}.
+
+create_context(TemplatesDescr, Options, Logger) ->
+  try
+    {Dicts, Templates} = fast_xml:parse(TemplatesDescr, Options),
+    {ok, #context{dicts = Dicts, templates = Templates, logger = Logger, options = Options}}
+  catch
+    _:Err ->
+      Err
+  end.
+  
+reset_context(Context = #context{dicts = Dicts}) ->
+  Dicts1 = gb_trees:map(fun(_K, _V) -> [] end, Dicts),
+  {ok, Context#context{dicts = Dicts1}}.

61e550f94bbc2c5951ae569cb823cfe7fe490ea7
Spasennyi4ert <spasennyi4ert@gmail.com>
2014-09-03 19:40:11 +0400

    Update fast.erl

--- src/fast.erl
+++ src/fast.erl
@@ -23,3 +23,17 @@ init([_Id]) ->
   end,
   {ok, Context} = create_context({file, root() ++"/templates.xml"}, [], F),
   {ok, #context{} = Context}.
+
+handle_info({udp, _Socket, _Ip, _Port, Bin}, #context{} = Context) ->
+  case fast_segment:decode(Bin, Context) of
+    {ok, {TemplateName, Msg, Rest, _}} ->
+      io:format("~p ~p~n", [TemplateName, Msg]),
+      %{noreply, Context}
+      {ok, { _, _, _, Context1}} = fast_segment:decode(Rest, Context),
+      handle_info({udp, _Socket, _Ip, _Port, Rest}, Context1)
+  end;
+handle_info( _Info, State) ->
+  {noreply, State}.
+  
+terminate(normal, State) ->
+  {noreply, State}.

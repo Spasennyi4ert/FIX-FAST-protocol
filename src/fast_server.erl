635ebe4c8407d3ada704d0423e2a8cbed039d6c5
Anastasia A Subbotina <spasennyi4ert@gmail.com>
2014-10-11 15:25:57 +0400

    Update fast_server.erl

--- src/fast_server.erl
+++ src/fast_server.erl
@@ -3,6 +3,8 @@
 
 -compile(export_all).
 
+-include("../include/fast_context.hrl").
+
 -record(state, {
 	socket,
 	context
@@ -12,10 +14,16 @@
 }).
 
 
-
-
 start_link() ->
 	gen_server:start_link(?MODULE, [], []).
+
+root() ->
+	case code:lib_dir(fast) of
+		{error, _Error} ->
+			filename:dirname(code:which(?MODULE)) ++ "/../";
+			Root_ when is_list(Root_) ->
+				Root_
+	end.
 
 init([]) ->
 	
@@ -39,14 +47,33 @@ init([]) ->
 	{multicast_ttl, 30},
          {raw, 0, 39, Bin}
       ]),
-    {ok, Pid} = fast:start_link(self()),
-    ok = gen_udp:controlling_process(Socket, Pid),  %% 35
-	{ok, #state{socket = Socket}}.
+    ok = gen_udp:controlling_process(Socket, self()),  %% 35
+    F = fun([], _) ->
+    	ok;
+    (Err, Val) ->
+    	io:format("~p ~p~n", [Err, Val])
+    	end,
+    try
+    	{Dicts, Templates} = fast_xml:parse({file, root() ++ "/spec/templates.xml"}, []),
+	{ok, #context{dicts = Dicts, templates = Templates, logger = F, options = [], socket = Socket}}
+    catch
+	_:Err ->
+		Err
+    end.
 
 ip_to_binary(Ip) ->
 	list_to_binary(tuple_to_list(Ip)).
 
-
+handle_info({udp, _Socket, _Ip, _Port, Bin}, #context{} = Context) ->
+	case fast_segment:decode(Bin, Context) of
+		{ok, {TemplateName, Msg, Rest, _}} ->
+			io:format("~p ~p~n", [TemplateName, Msg]),
+			{ok, { _, _, _, Context1}} = fast_segment:decode(Rest, Context),
+			handle_info({udp, _Socket, _Ip, _Port, Rest}, Context1);
+		{error, Reason} ->
+			io:format("ERROR: ~p, Bin = ~p ~n", [Reason, Bin]),
+			exit(failed)
+	end;
 handle_info(_Info, State) ->
 	{noreply, State}.
 

b3f143eed797c8ec997ef0eb3d823aa83d03acd5
Anastasia S. Subbotina <spasennyi4ert@gmail.com>
2015-06-18 17:21:02 +0300

    Изменения в получении данных
    Изменения направлены на получение данных из нескольких источников.

--- src/fast_server.erl
+++ src/fast_server.erl
@@ -2,45 +2,39 @@
 -behaviour(gen_server).
 
 -compile(export_all).
-
+-include_lib("stdlib/include/ms_transform.hrl").
 -include("../include/fast_context.hrl").
 -include("../include/fast_server.hrl").
+-include("log.hrl").
 
-start_link(Name) ->
-	gen_server:start_link(?MODULE, Name, []).
+-define(NTH, 5).
+-define(ID, 10).
+-define(SEQ, 7).
+-define(SEQRPT, 7).
+-define(EXP, 18).
+-define(MANT, 19).
+-define(STEP, 16).
+-define(POINTX, 12).
+-define(POINTY, 16).
+-define(POINTZ, 17).
+
+start_link(Name, SecID) ->
+    gen_server:start_link(?MODULE, [Name, SecID], []).
 
 stop() ->
-	gen_server:call(?MODULE, stop).
+    gen_server:call(?MODULE, stop).
 
-connect(Pid, SendTo) ->
-    gen_server:call(Pid, {connect, SendTo}).
-
-root() ->
-  case code:lib_dir(fast) of
-    {error, _Error} -> filename:dirname(code:which(?MODULE)) ++ "/../";
-    Root_ when is_list(Root_) -> Root_
-  end.
-
-
-init(Name) ->
-%	ftrc:start(),
-    fast:start_task(fast_disp, {fast_fsm_decode, start_link, []}),
-    fast:run(fast_disp, [self()]),
+init([Name, SecID]) ->
     {ok, Options} = application:get_env(fast, Name),
     {source, Source} = lists:keyfind(source, 1, Options),
     {port, Port} = lists:keyfind(port, 1, Options),
     {group, Group} = lists:keyfind(group, 1, Options),
-%	Source = "172.27.129.77",%
-%	Port = "24027",%27
-%	Group = "239.192.7.27",%27
-
     {ok,SourceAddress} = inet:parse_address(Source),
     {ok,GroupAddress} = inet:parse_address(Group),
     GroupIp = ip_to_binary(GroupAddress),
     LocalIp = ip_to_binary({0,0,0,0}),
     SourceIp = ip_to_binary(SourceAddress),
     Bin = << GroupIp/binary,LocalIp/binary,SourceIp/binary >>,
-
     {ok, Socket} = gen_udp:open(erlang:list_to_integer(Port),
 				[
 				 inet,
@@ -51,92 +45,92 @@ init(Name) ->
 				 {raw, 0, 39, Bin}
 				]),
 
-    {ok, Pid} = gen_udp:controlling_process(Socket, self()),  
-    {ok, #server{socket = Socket, name = Name, pid = Pid}}.
+    ok = gen_udp:controlling_process(Socket, self()),  
+    {ok, #server{socket = Socket, name = Name, sec_id = SecID}}.
+
+handle_call(stop, _, P) ->
+    {stop, shutdown, stopped, P};
+handle_call(_Msg, _From, State) ->
+    {noreply, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info({udp, _Socket, _Ip, _Port, <<>>}, State) ->
+    {noreply, State};
+handle_info({udp, _Socket, _Ip, _Port, Bin}, #server{name = Feed, sec_id = SecID} = State) ->
+    List = lists:reverse(lists:foldl(fun parse/2, [<<>>], [<<C:8>> || <<C:8>> <= Bin])),
+    if length(List) > 8 ->
+	    case lists:nth(?ID, List) of
+		SecID ->
+		    case Feed of
+			feed_a ->
+			    %?D (List),
+			    L = dig(List),
+			    extract(L);
+			    
+			    %fast_server_decode:feed(T);
+			feed_b ->
+			    L = dig(List),
+			    extract(L);
+			    %fast_server_decode:feed(T);
+			feed_c ->
+			    T = list_to_tuple([lists:nth(X, List) || X <- [?SEQRPT, ?EXP, ?MANT]]),
+			    fast_server_decode:feed(T);
+			feed_d ->
+			    T = list_to_tuple([lists:nth(X, List) || X <- [?SEQRPT, ?EXP, ?MANT]]),
+			    fast_server_decode:feed(T);
+			_ ->
+			    ok
+		    end;
+		_ ->
+		    ok
+	    end;
+       true ->
+	    ok
+    end,
+    {noreply, State};   
+	
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+terminate(_, _) ->
+    ok.
 
 ip_to_binary(Ip) ->
 	list_to_binary(tuple_to_list(Ip)).
 
-handle_call({connect, SendTo}, _From, #server{}) ->
-    Connected = #server{pid_fsm = SendTo},
-    {noreply, SendTo, Connected};
-handle_call(stop, _, P) ->
-	{stop, shutdown, stopped, P}.
+parse(<<0:1, C:7>>, [Cur|Acc]) ->
+    [<<0:1, Cur/binary, C:7>> | Acc];
+parse(<<1:1, C:7>>, [Cur|Acc]) ->
+    [<<>>, binary:decode_unsigned(<<0:1, Cur/binary, C:7>>) | Acc].
 
-handle_info({udp, _Socket, _Ip, _Port, <<>>}, _) ->
-	ok;
-%handle_info({udp, _Socket, _Ip, _Port, << _Head:16, Bin/binary>>}, #server{name = Name}) ->
-%	F = fun([], _) ->
-%		ok;
-%	(Err, Val) ->
-%		io:format("~p ~p~n", [Err, Val])
-%	end,
-%	{ok, Context} = fast:create_context({file, root() ++"/spec/templates.xml"}, [], F),
-%	case fast_segment:decode(Bin, Context) of
-%		{ok, {TemplateName, Msg, _Rest, _}} ->
-%			extract_date(TemplateName, Msg, Name),
-%			io:format("~p ~p ~p~n", [TemplateName, Msg, Name]),
-%			{noreply,#server{buffer = Bin, name = Name}};
-%		{error, Reason} ->
-%			io:format("ERROR: ~p, Bin = ~p ~n", [Reason, Bin]),
-%			exit(failed)
-%		
-%	end;
-handle_info({udp, _Socket, _Ip, _Port, Bin}, #server{name = Name, pid = Pid, pid_fsm = PidTo} = State) ->
-    fast_fsm_decode:feed(PidTo, {Pid, Name, Bin}),
-    {noreply, State};
-	
-handle_info(_Info, State) ->
-	{noreply, State}.
+dig(Bi) ->
+	lists:zip3([lists:nth(X, Bi) || X <- lists:reverse(list_x(lists:nth(?SEQ,Bi),?POINTX))],
+		    [lists:nth(Y, Bi) || Y <- lists:reverse(list(lists:nth(?SEQ,Bi),?POINTY))],
+		    [lists:nth(Z, Bi) || Z <- lists:reverse(list(lists:nth(?SEQ,Bi),?POINTZ))]).
 
-%terminate(normal, State) ->
-%	ftrc:stop().
-%	{noreply, State}.
-terminate(_,_) ->
-	ok.
+list_x(N,P) when N > 1 ->
+    M = N - 1,
+    [?STEP*(M)+P+1|list_x(N-1,P)];
+list_x(N,P) when N == 1 ->
+    M = N - 1,
+    [?STEP*(M)+P|list_x(N-1,P)];
+list_x(_N,_P) ->
+    [].
 
-extract_date(TemplateName, Msg, Name) when TemplateName == <<"DefaultIncrementalRefreshMessage">> ->
-	Trade = [Deal || {<<"MDEntries">>, Deal} <- Msg],
-	deeg(Trade, Name);
-%	io:format(" ~p~n ~n", [Trade]);
-extract_date(TemplateName, Msg, Name) ->
-	io:format("~p ~p ~p~n ~n", [TemplateName, Msg, Name]).
+list(N,P) when N >= 1 ->
+    M = N - 1,
+    [?STEP*(M)+P|list(N-1,P)];
+list(_N,_P) ->
+    [].
 
-deeg([[H|T]], Name) when is_list(H) andalso T == [] ->
-	filter(H, Name);
-deeg([[H|T]], Name) when is_list(H) ->
-%	io:format(" ~p~n ~n", [Trade]),
-	filter(H, Name),
-	deeg([T], Name).
-
-filter(L, Name) ->
-	case lists:member({<<"MDUpdateAction">>,0},L) of
-		true ->
-			case lists:member({<<"MDEntryType">>,<<"2">>},L) of
-				true ->
-					case lists:member({<<"SecurityID">>,72994630},L) of
-						true ->
-							Raw_Price = insert(L),%
-							Price = remake(Raw_Price),
-%							fast_index:get_data(Price),
-							fast_ind:get_data(Price),
-							io:format("~p ~p~n ~n", [Price, Name]);
-						false ->
-							{ok, nothing}
-					end;
-				false ->
-					{ok, nothing}
-			end;
-		false ->
-			{ok, nothing}
-	end.
-
-insert(L) ->
-	[Price || {<<"MDEntryPx">>,Price} <- L].
-
-remake([{Mantissa, Exponent}]) ->
-	Price = Mantissa * math:pow(10,Exponent),
-	Price.
-
-feed({Name, Bin}) ->
-    io:format("~p ~p~n ~n", [Bin, Name]).
+extract([H|T]) when is_tuple(H) andalso T == [] ->
+    fast_server_decode:feed(H);
+extract([H|T]) when is_tuple(H) ->
+    %?D (H),
+    fast_server_decode:feed(H),
+    extract(T).

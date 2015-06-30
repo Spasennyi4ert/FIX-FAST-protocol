db1ab89c0d75ce96f1884b84164a726c80949b3c
Anastasia S. Subbotina <spasennyi4ert@gmail.com>
2015-06-30 17:47:57 +0300

    Добавлен "стабилизатор"
    Добавлен "стабилизатор" для сообщений с номером, резко отличающимся от текущего

--- src/fast_server_decode.erl
+++ src/fast_server_decode.erl
@@ -11,41 +11,54 @@
 -define(SEQ, 7).
 
 -record(state, {
-	table,
-	seq
+	  table,
+	  seq = 0,
+	  count = 1,
+	  count_dup = 1
 }).
 
 start_link() ->
-	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 stop() ->
-	gen_server:call(?MODULE, stop).
+    gen_server:call(?MODULE, stop).
 
 feed(T) ->
-	gen_server:cast(?MODULE, {feed, T}).
+    gen_server:cast(?MODULE, {feed, T}).
 
 init([]) ->
-	ftrc:start(),
-	Tid = ets:new(data,[ordered_set, public]),
+    ftrc:start(),
+    Tid = ets:new(data,[ordered_set, public]),
     {ok,  #state{ table = Tid}}.
 
 handle_call(stop, _, P) ->
-	{stop, shutdown, stopped, P};
+    {stop, shutdown, stopped, P};
 handle_call(_Msg, _From, State) ->
-	{noreply, State}.
+    {noreply, State}.
 
-handle_cast({feed, {SeqRpt, Expo, Mant}}, #state{table = Tid, seq = SeqNum} = State) ->
-	case ets:lookup(Tid,SeqRpt) of
+handle_cast({feed, {SeqRpt, Expo, Mant}}, #state{table = Tid, seq = SeqNum, count = Count, count_dup = CD} = State) ->
+    case ets:lookup(Tid,SeqRpt) of
 	[] ->
-	    ets:insert(Tid, {SeqRpt,  Expo, Mant}),
-	    Last = ets:last(Tid),
-	    [{Se, Expo, Mantissa}] = ets:lookup(Tid, Last),
-	    price(Se, Expo, Mantissa),
-	    ets:delete(Tid,SeqNum),
-	    {noreply, #state{table = Tid, seq = SeqRpt - 5}};
-	[{_, _, _}] ->
-	    ok,
-	    {noreply, State};
+	    if SeqRpt == Count ->
+		    ets_insert(Tid, {SeqRpt, Expo, Mant}),
+		    Last = ets_last(Tid),
+		    [{Se, Expo, Mantissa}] = ets_lookup(Tid, Last),
+		    price(Se, Expo, Mantissa),
+		    ets_delete(Tid,SeqNum),
+		    {noreply, #state{table = Tid, seq = SeqRpt - 1, count = SeqRpt + 1, count_dup = CD}};
+	       SeqRpt > Count ->
+		    if SeqRpt == CD ->
+			    {noreply, #state{table = Tid, seq = SeqNum, count = SeqRpt + 1, count_dup = CD + 1}};
+		       SeqRpt >= CD ->
+			    {noreply, #state{table = Tid, seq = SeqNum, count = SeqRpt, count_dup = CD + 1}};
+		       true ->
+			    ok,
+			    {noreply, State}
+		    end;
+	       true ->
+		    ok,
+		    {noreply, State}
+	    end;
 	_ ->
 	    ok,
 	    {noreply, State}
@@ -59,12 +72,25 @@ handle_info(_Info, State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-terminate(_, _) ->
-	ftrc:stop(),
+terminate(_, #state{table = Tid}) ->
+    ets:delete(Tid),
+    ftrc:stop(),
     ok.
 
+ets_insert(Tid, {SeqRpt,  Expo, Mant}) ->
+    ets:insert(Tid, {SeqRpt,  Expo, Mant}).
+
+ets_last(Tid) ->
+    ets:last(Tid).
+
+ets_lookup(Tid, Last) ->
+    ets:lookup(Tid, Last).
+
+ets_delete(Tid,SeqNum) ->
+    ets:delete(Tid,SeqNum).
+
 price(Se, Expo, Mantissa) ->
-	Exponenta = Expo - 1,
+    Exponenta = Expo - 1,
     Price = Mantissa * math:pow(10, Exponenta),
-	?D ({Se, Price}).
-    %fast_ind:get_data(Price).
+    ?D ({Se, Price}),
+    fast_ind:get_data(Price).

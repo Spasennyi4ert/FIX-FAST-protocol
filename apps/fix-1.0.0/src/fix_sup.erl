3b5aa2d2c548a7c5bdc516776456c71a50ef20ed
Sergey A. Subbotin <c0mp2e5502@gmail.com>
2015-10-13 09:47:37 +0300

    Супервизор подключения
    Супервизор fix подключения

---
+++ apps/fix-1.0.0/src/fix_sup.erl
@@ -0,0 +1,15 @@
+-module(fix_sup).
+-behaviour(supervisor).
+
+%% API
+-export([start_link/2]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+start_link(Feed, Item) ->
+    supervisor:start_link({local, Feed}, ?MODULE, [Feed, Item]).
+
+init([Feed, Item]) ->
+    {ok, {{one_for_one, 5, 3600},
+     [{fix, {fix_exec_conn, start_link, [Feed, Item]}, permanent, 5000, worker, [fix_exec_conn]}]}}.

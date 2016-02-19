401846540c81d31563d8d2cf816971ec925741fb
Sergey A. Subbotin <c0mp2e5502@gmail.com>
2016-02-19 09:58:48 +0300

    Update fast_dispatcher.erl

--- apps/fast-1.0.0/src/fast_dispatcher.erl
+++ apps/fast-1.0.0/src/fast_dispatcher.erl
@@ -2,25 +2,35 @@
 -behaviour(gen_server).
 
 -record(state, {
+	  item,
 	  table,
 	  seq = 0,
 	  count = 1,
 	  count_dup = 1
 }).
 
--export([start_link/0, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2, init/1]).
+-export([start_link/2, feed/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2, init/1]).
 
-start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+start_link(Type, Item) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [Type, Item], []).
 
-init([]) ->
+feed(Msg) ->
+    gen_server:cast(?MODULE, {feed, Msg}).
 
-    Tid = ets:new(data,[ordered_set, public]),
-    {ok,  #state{ table = Tid}}.
+init([Type, Item]) ->
+    Tid = ets:new(Type, [ordered_set, public]),
+    {ok,  #state{item = Item, table = Tid}}.
 
 handle_call(_Msg, _From, State) ->
     {noreply, State}.
 
+handle_cast({feed, {Act, Type, SecID, Rpt, Exp, Mant}}, S = #state{item = Item}) ->
+    case SecID of
+	Item ->
+	    processed({Rpt, Act, Type, Exp, Mant}, S);
+	_ ->
+	    {noreply, S}
+    end;
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -33,3 +43,39 @@ code_change(_OldVsn, State, _Extra) ->
 terminate(_, #state{table = Tid}) ->
     ets:delete(Tid),
     ok.
+
+processed({Rpt, Act, Type, Exp, Mant}, S = #state{table = Tid, seq = Seq, count = Count, count_dup = CD}) ->
+    case ets:lookup(Tid, Rpt) of
+	[] ->
+	    if Rpt == Count andalso Act == 0 andalso Type == 50 ->
+		    %io:format("Msg : ~p~n", [{Rpt, Exp, Mant}]),
+		    ets:insert(Tid, {Rpt, Exp, Mant}),
+		    price(Exp, Mant),
+		    ets:delete(Tid, Seq),
+		    {noreply,S#state{seq = Rpt, count = Rpt + 1}};
+	       Rpt == Count ->
+		    {noreply,S#state{count = Rpt + 1}};
+	       Rpt > Count ->
+		    if Rpt == CD ->
+			    {noreply,S#state{count = Rpt + 1, count_dup = CD + 1}};
+		       Rpt > CD ->
+			    {noreply,S#state{count_dup = Rpt + 1}};
+		       true ->
+			    {noreply, S}
+		    end;
+	       true ->
+		    {noreply, S}
+	    end;
+	[_] ->
+	    {noreply, S}
+    end.
+
+price(Exp, Mant) ->
+    Price = case Exp of
+		123 ->
+		    Mant * math:pow(10, -5);
+		_ ->
+		    ok
+	    end,
+    ars_handler:get_data(Price),
+    io:format("Price: ~p~n", [Price]).

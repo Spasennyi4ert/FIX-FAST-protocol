95a7b3578011740d57897720a6537cd321518087
Sergey A. Subbotin <c0mp2e5502@gmail.com>
2016-05-01 18:17:45 +0400

    Update fast_dispatcher_sup.erl

--- apps/fast-1.0.0/src/fast_dispatcher_sup.erl
+++ apps/fast-1.0.0/src/fast_dispatcher_sup.erl
@@ -1,11 +1,11 @@
 -module(fast_dispatcher_sup).
 -behaviour(supervisor).
 
--export([start_link/0, init/1]).
+-export([start_link/2, init/1]).
 
-start_link() ->
-    supervisor:start_link({local, dispatch}, ?MODULE, []).
+start_link(Type, Item) ->
+    supervisor:start_link({local, Type}, ?MODULE, [Type, Item]).
 
-init([]) ->
-    Child = {dispatch, {fast_dispatcher, start_link, []}, permanent, 5000, worker, [fast_dispatcher]},
+init([Type, Item]) ->
+    Child = [{Type, {fast_dispatcher, start_link, [Type, Item]}, permanent, 5000, worker, [fast_dispatcher]}],
     {ok, {{one_for_one, 1, 3600}, Child}}.

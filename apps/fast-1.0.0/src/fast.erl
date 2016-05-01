1422945df1e365309876b7f4b9698d660829c8a3
Sergey A. Subbotin <c0mp2e5502@gmail.com>
2016-05-01 18:13:41 +0400

    Update fast.erl

--- apps/fast-1.0.0/src/fast.erl
+++ apps/fast-1.0.0/src/fast.erl
@@ -1,14 +1,16 @@
--module(fast).
--export([start_server/2, stop_server/1, start_dispatcher/0, stop_dispatcher/0]).
 
-start_server(Feed,SecID) ->
-    fast_supersup:start_server(Feed,SecID).
+-module(fast).
+
+-export([start_server/2, stop_server/1, start_dispatcher/2, stop_dispatcher/1]).
+
+start_server(Feed,Item) ->
+    fast_supersup:start_server(Feed,Item).
 
 stop_server(Feed) ->
     fast_supersup:stop_server(Feed).
 
-start_dispatcher() ->
-    fast_supersup:start_server().
+start_dispatcher(Type, Item) ->
+    fast_supersup:start_dispatcher(Type, Item).
 
-stop_dispatcher() ->
-    fast_supersup:stop_server().
+stop_dispatcher(Type) ->
+    fast_supersup:stop_dispatcher(Type).
